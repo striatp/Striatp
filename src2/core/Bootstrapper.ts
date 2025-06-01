e2e66db41c7bfeea105c8ab21b4c2ba60539a3c2
Sébastien <sebastien.works@outlook.com>
2025-06-01 23:10:59 +0200

    Create Bootstrapper.ts

---
+++ src2/core/Bootstrapper.ts
@@ -0,0 +1,18 @@
+import { ForgeCommand } from '../managers';
+import { CommandRegistry } from '../core';
+
+import { Commands } from '../commands';
+import { version } from '../../package.json';
+
+import type { BootstrapOptions } from '...';
+
+// Make so it checks the cache and based
+// on the current cache, it setup options
+
+interface BootstrapperScheme {
+  initialize?(): void;
+}
+
+export class Bootstrapper {
+  constructor(options?: 
+}
